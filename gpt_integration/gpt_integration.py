6f96665f48382a83ebeda0f1c6cf07e008f1b731
Your Name <osielscamacho@gmail.com>
2025-02-15 16:22:43 -0700

    Changed the Python code to match Johns env file

--- gpt_integration/gpt_integration.py
+++ gpt_integration/gpt_integration.py
@@ -15,7 +15,7 @@ def connect_db():
         connection = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASS"),
+            password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME")
         )
         print("Successfully Extracted Data")
